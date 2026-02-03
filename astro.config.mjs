0c453d215a60551a1b275f0ea597010524b7333a
Sam Harper <samuelrharper@gmail.com>
2026-02-02 23:00:51 -0500

    Update hero and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sharper2.github.io',
-    base: '/portfolio2026/'
+    base: '/portfolio2026/',
+    build: {
+        assets: '_astro_v2'
+    }
 });
