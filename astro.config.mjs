ff1df0f90eae62d927aa31ed09b3c5cdb407336f
Sam Harper <samuelrharper@gmail.com>
2026-02-03 15:00:23 -0500

    Remove _astro_v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sharper2.github.io',
-    base: '/portfolio2026/',
-    build: {
-        assets: '_astro_v2'
-    }
+    base: '/portfolio2026/'
 });
