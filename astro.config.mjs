933b20c7107b0cf26bb44b11cda21c94a39d4b2a
Sam Harper <samuelrharper@gmail.com>
2026-02-04 21:25:34 -0500

    Added HTTPS to URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'samharper.net',
+    site: 'https://samharper.net',
     base: '/portfolio2026/'
 });
