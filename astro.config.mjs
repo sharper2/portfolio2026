a6fcddbb2d917fd5b7b5d96faa307874461e2f52
Sam Harper <samuelrharper@gmail.com>
2026-02-04 21:31:05 -0500

    Updated mjs to not include base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://samharper.net',
-    base: '/portfolio2026/'
+    output: 'static'
 });
